008227fb8edbd7b73da2141a770cc07d565f3b35
bramsegers <bramsegers@hotmail.com>
2019-08-24 23:59:00 +0200

    new upload

--- p051_p100/pe071.cpp
+++ p051_p100/pe071.cpp
@@ -4,13 +4,13 @@ int n=3, d=7;
 int N=1000000;
 
 int main(){
-    long long i,c,n2=0,d2=1;
+	long long i,c,n2=0,d2=1;
 	for(i=N;i>1;i--){
-    	c=i*n/d;
+		c=i*n/d;
 		if(c*d2>=i*n2 && i%d>0){
 			n2=c;
 			d2=i;
 		}
-    }
+	}
 	std::cout << n2 << "/" << d2 << "\n";
 }
