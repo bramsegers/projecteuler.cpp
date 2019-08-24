9fcfad7cc54c496cdf4a296bed2f4ebba9c287fc
bramsegers <bramsegers@hotmail.com>
2019-08-24 23:56:44 +0200

    new upload

--- p051_p100/pe071.cpp
+++ p051_p100/pe071.cpp
@@ -1,17 +1,16 @@
-#include <stdio.h>
-using LL=long long;
+#include <iostream>
 
-LL n=3, d=7;
-LL N=1000000;
+int n=3, d=7;
+int N=1000000;
 
 int main(){
-    LL n2=0,d2=1;
-	for(LL i=N;i>1;i--){
-    	LL c=n*i/d;
+    long long i,c,n2=0,d2=1;
+	for(i=N;i>1;i--){
+    	c=i*n/d;
 		if(c*d2>=i*n2 && i%d>0){
 			n2=c;
 			d2=i;
 		}
     }
-	printf("%lld/%lld\n",n2,d2);
+	std::cout << n2 << "/" << d2 << "\n";
 }
